f8c3f22f4d1557404b349538183b9928fcd439e1
TusharMinche <tusharminche@gmail.com>
2025-11-25 22:26:13 +0530

    Live backend link added

--- frontend/src/redux/slices/inventorySlice.js
+++ frontend/src/redux/slices/inventorySlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/inventory';
+const API_URL = 'https://billing-software-mern-one.vercel.app/api/inventory';
 
 // Get token from state
 const getConfig = (token) => ({
